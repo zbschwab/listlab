04ad1e594c8210a755a0944390fd53e174e6ffd3
Z Schwab <schwabz@grinnell.edu>
2025-09-22 15:16:45 -0500

    wrote unit tests for arraylist

--- src/test/java/edu/grinnell/csc207/listlab/ArrayListTests.java
+++ src/test/java/edu/grinnell/csc207/listlab/ArrayListTests.java
@@ -24,6 +24,40 @@ public class ArrayListTests {
         assertEquals(10, list.size());
     }
 
+    @Test
+    public void listGetIndex10() {
+        ArrayList list = new ArrayList();
+        for (int i = 0; i < 20; i++) {
+            list.add(i);
+        }
+        assertEquals(9, list.get(10));
+    }
+
+    @Test
+    public void listGetNegativeIndex() {
+        ArrayList list = new ArrayList();
+        for (int i = 0; i < 20; i++) {
+            list.add(i);
+        }
+        assertEquals(19, list.get(-1));
+    }
+
+    @Test
+    public void listRemoveLast() {
+        ArrayList list = new ArrayList();
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+        assertEquals(2, list.remove(3));
+    }
+
+    // @Test
+    // public void listRemoveFromEmpty() {
+    //     ArrayList list = new ArrayList();
+    
+    //     assertEquals(2, list.remove(3));
+    // }
+
     @Property
     public boolean listAddSize(@ForAll @IntRange(min = 0, max = 1000) int sz) {
         ArrayList list = new ArrayList();
@@ -31,5 +65,15 @@ public class ArrayListTests {
             list.add(i);
         }
         return list.size() == sz;
+    }
+
+    @Property
+    public boolean listRemove1(@ForAll @IntRange(min = 0, max = 1000) int sz) {
+        ArrayList list = new ArrayList();
+        for (int i = 0; i < sz; i++) {
+            list.add(i);
+        }
+        list.remove(1);
+        return list.size() == (sz-1);
     }
 }
