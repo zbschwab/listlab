f2743f2af8d9edba86dd46b7fdd11d706d325736
Z Schwab <schwabz@grinnell.edu>
2025-09-22 15:51:52 -0500

    implemented size and add in arraylist

--- src/main/java/edu/grinnell/csc207/listlab/ArrayList.java
+++ src/main/java/edu/grinnell/csc207/listlab/ArrayList.java
@@ -1,23 +1,37 @@
 package edu.grinnell.csc207.listlab;
 
+import java.util.Arrays;
+
 /**
  * An array-based implementation of the list ADT.
  */
 public class ArrayList {
+    private int[] arrlist;
+    private int size = 0;
+
+    public ArrayList () {
+        this.arrlist = new int[10];
+        this.size = arrlist.length;
+    }
+    
     /**
      * Adds <code>value</code> to the end of the list
      * 
      * @param value the value to add to the end of the list
      */
     public void add(int value) {
-        throw new UnsupportedOperationException("Unimplemented method 'add'");
+        if (size == arrlist.length) {
+            arrlist = Arrays.copyOf(arrlist, arrlist.length * 2);
+        }
+        arrlist[size] = value;
     }
 
     /**
      * @return the number of elements in the list
      */
     public int size() {
-        throw new UnsupportedOperationException("Unimplemented method 'size'");
+        
+        return size;
     }
 
     /**
